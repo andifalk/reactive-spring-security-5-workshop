05928e223b1fd78bd374044f4ab5f1303bee60b0
Andreas Falk <andreas.falk@novatec-gmbh.de>
2018-10-19 09:44:58 +0200

    add reactive playground

---
+++ reactive-playground/README.md
@@ -0,0 +1,7 @@
+# Spring Security 5 Workshop
+
+## Reactive Playground
+
+This contains a starting point to play around with the reactive stuff.
+
+Just add tests to class _ReactiveTest_ to learn the reactive streams api.
---
+++ reactive-playground/src/test/java/com/example/ReactiveTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class ReactiveTest {
+
+    @Test
+    void testSimple() {
+        Flux<String> aFlux = Flux.just("MyString");
+        //Flux<String> aFlux = Flux.just("MyString").log();
+
+        aFlux.subscribe(
+                f -> System.out.println("Here's some value: " + f)
+        );
+
+        //StepVerifier.create(aFlux).expectNext("MyString").verifyComplete();
+    }
+
+}
