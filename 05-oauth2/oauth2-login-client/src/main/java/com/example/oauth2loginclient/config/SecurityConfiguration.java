c18e8f6ed5a4a9b8bd433a3e6daeb38336da0d16
Andreas Falk <andreas.falk@novatec-gmbh.de>
2019-02-13 22:44:03 +0100

    update workshop tutorial

--- 05-oauth2/oauth2-login-client/src/main/java/com/example/oauth2loginclient/config/SecurityConfiguration.java
+++ 05-oauth2/oauth2-login-client/src/main/java/com/example/oauth2loginclient/config/SecurityConfiguration.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
