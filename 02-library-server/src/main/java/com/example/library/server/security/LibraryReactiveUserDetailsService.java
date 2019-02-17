ece4b445d8095459802807bd6be772024c6d6e39
Andreas Falk <andreas.falk@novatec-gmbh.de>
2019-02-17 18:34:05 +0100

    add password upgrades

--- 02-library-server/src/main/java/com/example/library/server/security/LibraryReactiveUserDetailsService.java
+++ 02-library-server/src/main/java/com/example/library/server/security/LibraryReactiveUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.example.library.server.security;
 
 import com.example.library.server.business.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsPasswordService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +12,8 @@ import reactor.core.publisher.Mono;
 @Service
 public class LibraryReactiveUserDetailsService implements ReactiveUserDetailsService, ReactiveUserDetailsPasswordService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LibraryReactiveUserDetailsService.class);
+
     private final UserService userService;
 
     public LibraryReactiveUserDetailsService(UserService userService) {
@@ -18,11 +22,20 @@ public class LibraryReactiveUserDetailsService implements ReactiveUserDetailsSer
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
+        LOGGER.info("Finding user for user name {}", username);
+
         return userService.findOneByEmail(username).map(LibraryUser::new);
     }
 
+
     @Override
     public Mono<UserDetails> updatePassword(UserDetails user, String newPassword) {
+
+        LOGGER.warn("Password upgrade for user with name '{}'", user.getUsername());
+
+        // Only for demo purposes. NEVER log passwords in production!!!
+        LOGGER.info("Password upgraded from '{}' to '{}'", user.getPassword(), newPassword);
+
         return userService.findOneByEmail(user.getUsername())
                 .doOnSuccess(u -> u.setPassword(newPassword))
                 .flatMap(userService::update)
