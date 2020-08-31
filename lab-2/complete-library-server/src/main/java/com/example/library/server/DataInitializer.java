2ec817e7eb0dd8a49e423a1085d33f06ee0371de
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-08-31 21:44:52 +0200

    update data initializer

--- lab-2/complete-library-server/src/main/java/com/example/library/server/DataInitializer.java
+++ lab-2/complete-library-server/src/main/java/com/example/library/server/DataInitializer.java
@@ -117,68 +117,68 @@ public class DataInitializer implements CommandLineRunner {
   private void createBooks() {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    logger.info("Creating some books...");
+
     userRepository
         .findOneByEmail("bruce.wayne@example.com")
+        .single()
+        .flatMap(
+            user ->
+                Flux.just(
+                        new Book(
+                            BOOK_CLEAN_CODE_IDENTIFIER,
+                            "9780132350884",
+                            "Clean Code",
+                            "Even bad code can function. But if code isn’t clean, it can bring a development "
+                                + "organization to its knees. Every year, countless hours and significant resources are "
+                                + "lost because of poorly written code. But it doesn’t have to be that way. "
+                                + "Noted software expert Robert C. Martin presents a revolutionary paradigm with Clean Code: "
+                                + "A Handbook of Agile Software Craftsmanship . Martin has teamed up with his colleagues from "
+                                + "Object Mentor to distill their best agile practice of cleaning code “on the fly” into a book "
+                                + "that will instill within you the values of a software craftsman and make you a better "
+                                + "programmer—but only if you work at it.",
+                            Collections.singletonList("Bob C. Martin"),
+                            true,
+                            user),
+                        new Book(
+                            BOOK_CLOUD_NATIVE_IDENTIFIER,
+                            "9781449374648",
+                            "Cloud Native Java",
+                            "What separates the traditional enterprise from the likes of Amazon, Netflix, "
+                                + "and Etsy? Those companies have refined the art of cloud native development to "
+                                + "maintain their competitive edge and stay well ahead of the competition. "
+                                + "This practical guide shows Java/JVM developers how to build better software, "
+                                + "faster, using Spring Boot, Spring Cloud, and Cloud Foundry.",
+                            Arrays.asList("Josh Long", "Kenny Bastiani"),
+                            false,
+                            null),
+                        new Book(
+                            BOOK_SPRING_ACTION_IDENTIFIER,
+                            "9781617291203",
+                            "Spring in Action: Covers Spring 4",
+                            "Spring in Action, Fourth Edition is a hands-on guide to the Spring Framework, "
+                                + "updated for version 4. It covers the latest features, tools, and practices "
+                                + "including Spring MVC, REST, Security, Web Flow, and more. You'll move between "
+                                + "short snippets and an ongoing example as you learn to build simple and efficient "
+                                + "J2EE applications. Author Craig Walls has a special knack for crisp and "
+                                + "entertaining examples that zoom in on the features and techniques you really need.",
+                            Collections.singletonList("Craig Walls"),
+                            false,
+                            null),
+                        new Book(
+                            BOOK_DEVOPS_IDENTIFIER,
+                            "9781942788003",
+                            "The DevOps Handbook",
+                            "Wondering if The DevOps Handbook is for you? Authors, Gene Kim, Jez Humble, "
+                                + "Patrick Debois and John Willis developed this book for anyone looking to transform "
+                                + "their IT organization—especially those who want to make serious changes through the "
+                                + "DevOps methodology to increase productivity, profitability and win the marketplace.",
+                            Arrays.asList("Gene Kim", "Jez Humble", "Patrick Debois"),
+                            false,
+                            null))
+                    .flatMap(bookRepository::save)
+                    .then(bookRepository.count()))
         .subscribe(
-            user -> {
-              logger.info("Creating some initial books...");
-              bookRepository
-                  .saveAll(
-                      Flux.just(
-                          new Book(
-                              BOOK_CLEAN_CODE_IDENTIFIER,
-                              "9780132350884",
-                              "Clean Code",
-                              "Even bad code can function. But if code isn’t clean, it can bring a development "
-                                  + "organization to its knees. Every year, countless hours and significant resources are "
-                                  + "lost because of poorly written code. But it doesn’t have to be that way. "
-                                  + "Noted software expert Robert C. Martin presents a revolutionary paradigm with Clean Code: "
-                                  + "A Handbook of Agile Software Craftsmanship . Martin has teamed up with his colleagues from "
-                                  + "Object Mentor to distill their best agile practice of cleaning code “on the fly” into a book "
-                                  + "that will instill within you the values of a software craftsman and make you a better "
-                                  + "programmer—but only if you work at it.",
-                              Collections.singletonList("Bob C. Martin"),
-                              true,
-                              user),
-                          new Book(
-                              BOOK_CLOUD_NATIVE_IDENTIFIER,
-                              "9781449374648",
-                              "Cloud Native Java",
-                              "What separates the traditional enterprise from the likes of Amazon, Netflix, "
-                                  + "and Etsy? Those companies have refined the art of cloud native development to "
-                                  + "maintain their competitive edge and stay well ahead of the competition. "
-                                  + "This practical guide shows Java/JVM developers how to build better software, "
-                                  + "faster, using Spring Boot, Spring Cloud, and Cloud Foundry.",
-                              Arrays.asList("Josh Long", "Kenny Bastiani"),
-                              false,
-                              null),
-                          new Book(
-                              BOOK_SPRING_ACTION_IDENTIFIER,
-                              "9781617291203",
-                              "Spring in Action: Covers Spring 4",
-                              "Spring in Action, Fourth Edition is a hands-on guide to the Spring Framework, "
-                                  + "updated for version 4. It covers the latest features, tools, and practices "
-                                  + "including Spring MVC, REST, Security, Web Flow, and more. You'll move between "
-                                  + "short snippets and an ongoing example as you learn to build simple and efficient "
-                                  + "J2EE applications. Author Craig Walls has a special knack for crisp and "
-                                  + "entertaining examples that zoom in on the features and techniques you really need.",
-                              Collections.singletonList("Craig Walls"),
-                              false,
-                              null),
-                          new Book(
-                              BOOK_DEVOPS_IDENTIFIER,
-                              "9781942788003",
-                              "The DevOps Handbook",
-                              "Wondering if The DevOps Handbook is for you? Authors, Gene Kim, Jez Humble, "
-                                  + "Patrick Debois and John Willis developed this book for anyone looking to transform "
-                                  + "their IT organization—especially those who want to make serious changes through the "
-                                  + "DevOps methodology to increase productivity, profitability and win the marketplace.",
-                              Arrays.asList("Gene Kim", "Jez Humble", "Patrick Debois"),
-                              false,
-                              null)))
-                  .log()
-                  .then(bookRepository.count())
-                  .subscribe(c -> logger.info("{} books created", c));
-            });
+            c -> logger.info("{} books created", c), t -> logger.error("Error {}", t.getMessage()));
   }
 }
